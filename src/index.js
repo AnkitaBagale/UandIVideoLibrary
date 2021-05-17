caf6ad769b1eade35a410dbc8ba3c01cd65c0c92
ankitabagale <manali.bagale@gmail.com>
2021-05-17 23:23:41 +0530

    fix: router scroll issue

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from "react-router-dom"; 
+import { BrowserRouter as Router } from 'react-router-dom';
 import { App } from './App';
-import { AuthenticationProvider, StateContextProvider } from "./Context";
+import { AuthenticationProvider, StateContextProvider } from './Context';
 import { LanguageContextProvider } from './Context';
-
-
+import ScrollToTop from './ScrollToTop';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <LanguageContextProvider>
-      <Router>
-        <AuthenticationProvider>
-          <StateContextProvider> 
-            <App />
-          </StateContextProvider>
-        </AuthenticationProvider>
-      </Router>
-    </LanguageContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<LanguageContextProvider>
+			<Router>
+				<AuthenticationProvider>
+					<StateContextProvider>
+						<ScrollToTop />
+						<App />
+					</StateContextProvider>
+				</AuthenticationProvider>
+			</Router>
+		</LanguageContextProvider>
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
-
-
