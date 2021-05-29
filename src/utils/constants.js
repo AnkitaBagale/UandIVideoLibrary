b9a07523dd49f4f9fafec273156b76f53a34175c
ankitabagale <manali.bagale@gmail.com>
2021-05-29 10:50:05 +0530

    fix: api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-//export const API_URL = 'https://uandistoreapi.herokuapp.com';
+export const API_URL = 'https://uandistoreapi.herokuapp.com';
 //dev url
-export const API_URL = 'http://localhost:3001';
+// export const API_URL = 'http://localhost:3001';
