199b8148fdaba0b7d691ec8749069850cc5d7303
ankitabagale <manali.bagale@gmail.com>
2021-07-19 21:26:22 +0530

    fix: image sizes

--- src/Components/HomePage/HomepageBanner.jsx
+++ src/Components/HomePage/HomepageBanner.jsx
@@ -1,25 +1,49 @@
-import { Link } from "react-router-dom";
-import "./css/banner.css";
+import { Link } from 'react-router-dom';
+import './css/banner.css';
 
-export const HomepageBanner = ({imgSrc, titleText, descText, btnText, btnLink, badgeText, reversed}) =>{
-    return(
-        <div>
-        <div className="banner-wrapper">
-            <div className="banner-50-50" style={{flexDirection: reversed?"row-reverse":"row" }}>
-                <span className="badge-container">
-                    <img src={imgSrc} className={reversed ? "banner-img-right" :"banner-img-left"} alt="banner" />
-                    <span className={`badge ${reversed? "bottom-right-position" : "bottom-right-position-reversed"} bg-secondary`}>{badgeText}</span>
-                </span>
+export const HomepageBanner = ({
+	imgSrc,
+	titleText,
+	descText,
+	btnText,
+	btnLink,
+	badgeText,
+	reversed,
+}) => {
+	return (
+		<div>
+			<div className='banner-wrapper'>
+				<div
+					className='banner-50-50'
+					style={{ flexDirection: reversed ? 'row-reverse' : 'row' }}>
+					<span className='badge-container'>
+						<img
+							loading='lazy'
+							src={imgSrc}
+							className={reversed ? 'banner-img-right' : 'banner-img-left'}
+							alt='banner'
+						/>
+						<span
+							className={`badge ${
+								reversed
+									? 'bottom-right-position'
+									: 'bottom-right-position-reversed'
+							} bg-secondary`}>
+							{badgeText}
+						</span>
+					</span>
 
-                <div className="text-container">
-                    <h3>{titleText}</h3>
-                    <p>{descText}</p>
-                    <div className="CTA-Container">
-                        <Link to={btnLink} className="btn btn-solid-primary">{btnText}</Link>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
-    )
-}
+					<div className='text-container'>
+						<h3>{titleText}</h3>
+						<p>{descText}</p>
+						<div className='CTA-Container'>
+							<Link to={btnLink} className='btn btn-solid-primary'>
+								{btnText}
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
