06e60818205a92eff1658ae1a856dabe8b285972
ankitabagale <manali.bagale@gmail.com>
2021-07-21 22:58:12 +0530

    fix: api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-// export const API_URL = 'https://uandistoreapi.herokuapp.com';
-export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://uandistoreapi.herokuapp.com';
+// export const API_URL = 'http://localhost:3000';
 export const SOCIAL_MEDIA_URL = 'https://u-and-i-talks.netlify.app';
 
 export const SHARE_POST_TITLE = encodeURIComponent(
